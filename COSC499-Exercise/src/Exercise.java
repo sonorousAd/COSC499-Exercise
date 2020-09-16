c46372b38ad6da24c7ee62bf4135f9bc1b7d2b4d
Adrian Morillo <adrianmorilloq@outlook.com>
2020-09-16 08:58:34 -0700

    commit exercise clean

--- COSC499-Exercise/src/Exercise.java
+++ COSC499-Exercise/src/Exercise.java
@@ -22,34 +22,16 @@ public class Exercise {
 
 		System.out.println("to print the odd numbers enter 'o' to print the even numbers enter'e'");
 		String str = scan.next();
-		
-		
-		
 
 		if (str.equals("e")) {
-			
+
 			System.out.println(ev.meven(arr));
 
-//			System.out.println("even");
-//			for (int j = 0; j < arr.length; j++) {
-//				if ((arr[j] % 2) == 0) {
-//					narr.add(arr[j]);
-//				}
-//			}
-//		System.out.println("The even numbers entered are" + Arrays.toString(narr.toArray()));
-
-		//}
 		if (str.equals("o")) {
 			System.out.println(od.modd(arr));
 
-			
-//			System.out.println("odd");
-//			for (int j = 0; j < arr.length; j++) {
-//				if ((arr[j] % 2) != 0) {
-//					narr.add(arr[j]);
-//				}
-//			}
-//			System.out.println("the odd numbers are" + Arrays.toString(narr.toArray()));
-		//}
+
+			}
+		}
 	}
 }
